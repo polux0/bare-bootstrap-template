dfb3b139a2eaa874e90ea05694f1e8c19b33e087
Aleksa Stojanović <afidelo@protonmail.com>
2018-12-26 23:59:14 +0100

    initial commit

---
+++ startbootstrap-bare-gh-pages/vendor/experiments/01.js
@@ -0,0 +1,7 @@
+$(function() {
+    console.log( "Someone is talentless!" );
+    console.log("It's time to fire up Immediately Inoked Function Expression");
+    (function EIFE(){
+    	console.log("IEFE fired up!")
+    })();
+});
